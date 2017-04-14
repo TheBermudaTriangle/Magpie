70bed5d82553875181b5fc0b374beb3c9c689ce3
torquecoder <torquecode@gmail.com>
2017-04-14 14:40:10 +0530

    Heroku Deployment

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.use(express.static('public'))
 
 //app.use(express.static(__dirname + '/public'));
 
-app.listen(function () {
+var portnumber = process.env.PORT || 8080;
+
+app.listen(portnumber, function () {
   console.log('Example app listening on port 3000!')
 })
