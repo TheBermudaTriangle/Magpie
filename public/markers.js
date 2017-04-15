3abbf762b61b88cf680f2b5e5d23e221ef57e8ab
torquecoder <torquecode@gmail.com>
2017-04-15 12:18:43 +0530

    working

--- public/markers.js
+++ public/markers.js
@@ -6,21 +6,8 @@ var makeMyMap = function(){
 	        navigator.geolocation.getCurrentPosition(showPosition);
 	    }
 	}
-<<<<<<< HEAD
-	currentPosJson={
-		lat: position.coords.latitude,
-		lng: position.coords.longitude
-	}
-	//console.log(currentPosJson);
-}
-function initMap() {
-	if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
-			currentDataJson={
-=======
 	function showPosition(position) {
 		currentDataJson={
->>>>>>> 3e10e73982af77e74813dcec9870275623759f52
 			lat: position.coords.latitude,
 			lng: position.coords.longitude,
 			username:"xcepti0n"
@@ -29,41 +16,6 @@ function initMap() {
 			lat: position.coords.latitude,
 			lng: position.coords.longitude
 		}
-<<<<<<< HEAD
-		//console.log(currentPosJson);
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-	      zoom: 8,
-	      center: currentPosJson
-	    });
-		//currentDataText = JSON.stringify(currentDataJson);
-		//console.log(currentDataText);
-	    $.post("https://magpie-server.herokuapp.com",currentDataJson,function(data){
-	    	//var data = JSON.parse(data);
-	    	//console.log(data);
-	    	// json=JSON.parse(data);
-	    	json=data;
-	    	//console.log(json);
-	    	var marker=[];
-	    	for(var i=0;i<data.length;i++){
-	    		var pos={
-						lat: Number(data[i].lat),
-						lng: Number(data[i].lng)
-					}
-				//console.log(pos);
-	    		marker[i] = new google.maps.Marker({
-		          position: pos,
-		          map: map,
-		          title:data[i].username
-		        });
-	    	}
-	    	//setTimeout(initMap,60000);
-
-	    });
-	  });
-    }
-  }
-=======
 		console.log(currentPosJson);
 	}
 	function initMap() {
@@ -78,27 +30,22 @@ function initMap() {
 				lat: position.coords.latitude,
 				lng: position.coords.longitude
 			}
-			//console.log(currentPosJson);        	
-	        
+
 	        var map = new google.maps.Map(document.getElementById('map'), {
 		      zoom: 8,
 		      center: currentPosJson
 		    });
-			//currentDataText = JSON.stringify(currentDataJson);
-			//console.log(currentDataText);
+
 		    $.post("https://magpie-server.herokuapp.com",currentDataJson,function(data){
-		    	//var data = JSON.parse(data);
-		    	//console.log(data);
-		    	// json=JSON.parse(data);
+
 		    	json=data;
-		    	//console.log(json);
 		    	var marker=[];
 		    	for(var i=0;i<data.length;i++){
 		    		var pos={
 							lat: Number(data[i].lat),
 							lng: Number(data[i].lng)
 						}
-					//console.log(pos);
+
 		    		marker[i] = new google.maps.Marker({
 			          position: pos,
 			          map: map,
@@ -112,4 +59,3 @@ function initMap() {
 	    }
 	  }
 }
->>>>>>> 3e10e73982af77e74813dcec9870275623759f52
