c0b3815f93e536390ff6d927120963a597d19453
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-04-18 21:48:23 +0530

    major changes incorporated

--- public/markers.js
+++ public/markers.js
@@ -1,99 +1,32 @@
-var timeoutRepeat=true;
 var map;
-function EraseTimeout(){
-	timeoutRepeat=false;
-	map=null;
-  	console.log("timeoutReapeat: "+timeoutRepeat+" map: "+map);
-  }
+var infoWindow;
+var toggleVar = 0;
 var makeMyMap = function(){
-	initMap();
-	var currentPosJson,currentDataJson;
-	function getLocation() {
-	    if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(showPosition);
-	    }
+	if(typeof latitude == 'undefined'){
+		setTimeout(makeMyMap,10000)
 	}
-	function showPosition(position) {
-		currentDataJson={
-			lat: position.coords.latitude,
-			lng: position.coords.longitude,
-			username:user__name
-		}
-		currentPosJson={
-			lat: position.coords.latitude,
-			lng: position.coords.longitude
-		}
-		console.log(currentPosJson);
+	else{
+		initMap();
 	}
-	function initMap() {
-		console.log(user__name +"continue: "+timeoutRepeat);
-		if(user__name && timeoutRepeat)
-		{
-			if (navigator.geolocation) {
-	        navigator.geolocation.getCurrentPosition(function(position){
-				currentDataJson={
-				lat: position.coords.latitude,
-				lng: position.coords.longitude,
-				username:user__name
-			}
-			currentPosJson={
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			}
-			//console.log(currentPosJson);        	
-	        
-	        map = new google.maps.Map(document.getElementById('map'), {
-		      zoom: 14,
-		      center: currentPosJson
-		    });
-
-
-	        var client_location = new google.maps.LatLng(currentDataJson.lat, currentDataJson.lng);
-	        var client_marker = new google.maps.Marker({
-								   position: client_location,
-								   map: map,
-								   });
-	        var infoWindow ;
-	        
-
-			//currentDataText = JSON.stringify(currentDataJson);
-			//console.log(currentDataText);
-
-			$("#submit").on("click",function(){
-				var dismsg = $("#message").val();
-				console.log(dismsg);
-				dismsg = user__name + " : " + dismsg;
-				infoWindow= new google.maps.InfoWindow({content:dismsg});
-				infoWindow.open(map, client_marker);
-				$("#message").val(" ");
-			})
-
-		    $.post("https://magpie-server.herokuapp.com",currentDataJson,function(data){
-		    	json=data;
-		    	//console.log(json);
-		    	var marker=[];
-		    	for(var i=0;i<data.length;i++){
-		    		var pos={
-							lat: Number(data[i].lat),
-							lng: Number(data[i].lng)
-						}
-					//console.log(pos);
-		    		marker[i] = new google.maps.Marker({
-			          position: pos,
-			          map: map,
-			          title:data[i].username
-			        });
-		    	}
-		    	
-		    	
-		    	setTimeout(initMap,60000);		    	
-
-		    });
-		  });
-		}
-
-	    }
-	  }
 }
-
-
+function initMap(){
+	map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: new google.maps.LatLng(latitude,longitude)
+    });
+}
+function addMarker(lat,lng,msg,user){
+	if(user == user__name && toggleVar == 1)
+	{
+		infoWindow.close();
+	}
+	toggleVar = 1;
+    var client_location = new google.maps.LatLng(lat,lng);
+    var client_marker = new google.maps.Marker({
+						   position: client_location,
+						   map: map,
+						   });
+    console.log('marker');
+	infoWindow= new google.maps.InfoWindow({content:user + " : " + msg});
+	infoWindow.open(map, client_marker);
+}
